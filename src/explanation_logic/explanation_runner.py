a95c951a9edd53679c390cb1fba8833d9bd3db87
Sarah Dribi Alaoui <sarah.dribi_alaoui@bordeaux-inp.fr>
2025-10-01 13:35:41 +0200

    message

--- src/explanation_logic/explanation_runner.py
+++ src/explanation_logic/explanation_runner.py
@@ -141,13 +141,4 @@ class ExplanationRunner:
             tag=tag,
         )
 
-from src.dataset.mnist_data import  TestLoaderMnist  
 
-runner = ExplanationRunner(model="models/mnist-10x2.onnx", dataset="mnist")
-runner.set_outputs_base("new_outputs").set_coarsening_timeout_step(20)
-
-# charger le test set
-loader = TestLoaderMnist()  # ou "breast_cancer"
-
-# expliquer le 0ème échantillon
-results = runner.explain_index(dataloader=loader, idx=0, tag="demo")
